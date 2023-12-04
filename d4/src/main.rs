b0def95b05eefd47ea96c44613512d11710be8e6
tobytaken <tobiassteinhaeusler@gmail.com>
2023-12-04 17:00:31 +0100

    Add d4p2

--- d4/src/main.rs
+++ d4/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::VecDeque;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
@@ -12,17 +13,35 @@ fn main() {
         .map(|line| line.unwrap())
         .collect();
 
-    one(&content);
-    two(&content);
+    let cards = parse_cards(&content);
+
+    one(&cards);
+    two(&cards);
 }
 
-fn one(content: &[String]) {
-    let cards = parse_cards(&content);
+fn one(cards: &Vec<Card>) {
     let score = get_score(&cards);
 
-    println!("Score: {}", score);
+    println!("Part 1: {}", score);
 }
-fn two(content: &[String]) {}
+
+fn two(cards: &Vec<Card>) {
+    let mut total: u32 = 0;
+    let mut queue: VecDeque<usize> = (0..cards.len()).collect();
+
+    while let Some(card_index) = queue.pop_front() {
+        total += 1;
+        let card = &cards[card_index];
+        let matches = get_matches(card);
+        for i in 0..matches {
+            let index = card_index + i as usize + 1;
+            if index < cards.len() {
+                queue.push_back(index);
+            }
+        }
+    }
+    println!("Part 2: {}", total);
+}
 
 fn parse_cards(content: &[String]) -> Vec<Card> {
     let mut cards: Vec<Card> = vec![];
@@ -51,15 +70,21 @@ fn parse_cards(content: &[String]) -> Vec<Card> {
 fn get_score(cards: &Vec<Card>) -> u32 {
     let mut score = 0;
     for card in cards {
-        let mut matches: u32 = 0;
-        for num in &card.win_nums {
-            if card.have_nums.contains(num) {
-                matches += 1;
-            }
-        }
+        let matches: u32 = get_matches(&card);
+
         if matches > 0 {
             score += 2u32.pow(matches - 1);
         }
     }
     score
+}
+
+fn get_matches(card: &Card) -> u32 {
+    let mut matches: u32 = 0;
+    for num in &card.win_nums {
+        if card.have_nums.contains(num) {
+            matches += 1;
+        }
+    }
+    matches
 }
