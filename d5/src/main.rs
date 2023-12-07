be553aa32817e76b6429442b29a14fac3d277846
tobytaken <tobiassteinhaeusler@gmail.com>
2023-12-07 12:37:19 +0100

    Add d5p2

--- d5/src/main.rs
+++ d5/src/main.rs
@@ -1,5 +1,5 @@
-use ::std::fs::File;
-use ::std::io::{BufRead, BufReader};
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 fn main() {
     let f = File::open("input.txt").unwrap();
     let content: Vec<String> = BufReader::new(f)
@@ -29,7 +29,25 @@ fn main() {
         acc
     });
 
+    let maps_num: Vec<Vec<Vec<u64>>> =
+        content.iter().skip(1).fold(vec![vec![]], |mut acc, line| {
+            if line.is_empty() {
+                acc.push(vec![]);
+                skip_next = true;
+            } else if skip_next {
+                skip_next = false;
+            } else {
+                let numbers = line
+                    .split_whitespace()
+                    .map(|num| num.parse::<u64>().unwrap())
+                    .collect::<Vec<u64>>();
+                acc.last_mut().unwrap().push(numbers);
+            }
+            acc
+        });
+
     one(&seeds, &maps);
+    two(&seeds, &maps_num);
 }
 
 fn one(seeds: &[u64], maps: &Vec<Vec<&str>>) {
@@ -54,4 +72,31 @@ fn one(seeds: &[u64], maps: &Vec<Vec<&str>>) {
     locations.sort_unstable();
     println!("{}", locations[0]);
 }
-fn two() {}
+fn two(seeds: &[u64], maps: &Vec<Vec<Vec<u64>>>) {
+    let mut locations: Vec<u64> = vec![];
+
+    for index in (0..seeds.len()).step_by(2) {
+        let start: usize = seeds[index] as usize;
+        let len: usize = seeds[index + 1] as usize;
+        for seed in start..start + len {
+            let mut current = seed as usize;
+            for map in &maps[1..] {
+                for line in map {
+                    if current >= line[1] as usize && current < line[1] as usize + line[2] as usize
+                    {
+                        current = current - line[1] as usize + line[0] as usize;
+                        break;
+                    }
+                }
+            }
+            locations.push(current as u64);
+        }
+    }
+
+    let mut min: u64 = u64::MAX;
+
+    for location in locations {
+        min = std::cmp::min(min, location);
+    }
+    println!("{}", min);
+}
